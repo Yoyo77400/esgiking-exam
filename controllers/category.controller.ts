c059f5af7f7fcd9edbc85b65cd7a494255c7f150
Yohan <yohan.georgelin@gmail.com>
2025-02-18 18:04:47 +0100

    create crud category

--- controllers/category.controller.ts
+++ controllers/category.controller.ts
@@ -24,6 +24,55 @@ export class CategoryController {
         const categoryService = mongooseService.categoryService;
         const category = await categoryService.createCategory(req.body);
         res.json(category);
+    }
 
+    async getCategory(req: express.Request, res: express.Response): Promise<void> {
+        if(!req.params.id) {
+            res.status(400).end();
+            return;
+        }
+        
+        const mongooseService = await MongooseService.getInstance();
+        const categoryService = mongooseService.categoryService;
+        const category = await categoryService.findCategoryById(req.params.id);
+        if (!category) {
+            res.status(404).end();
+            return;
+        }
+        res.json(category);
+    }
+
+    async deleteCategory(req: express.Request, res: express.Response): Promise<void> {
+        if(!req.params.id) {
+            res.status(400).end();
+            return;
+        }
+        
+        const mongooseService = await MongooseService.getInstance();
+        const categoryService = mongooseService.categoryService;
+        const category = await categoryService.deleteCategoryById(req.params.id);
+        if (!category) {
+            res.status(404).end();
+            return;
+        }
+        res.status(204).end();
+    }
+
+    buildRouter(): express.Router {
+        const router = express.Router();
+        router.post('/',
+            express.json(),
+            sessionMiddleware(), 
+            roleMiddleware(IEmployeeRole.MANAGER), 
+            this.createCategory);
+        router.get('/:id',
+            express.json(),
+            sessionMiddleware(), 
+            roleMiddleware(IEmployeeRole.MANAGER), 
+            this.getCategory);
+        router.delete('/:id', sessionMiddleware(), 
+        roleMiddleware(IEmployeeRole.ADMIN), 
+        this.deleteCategory);
+        return router;
     }
 }
